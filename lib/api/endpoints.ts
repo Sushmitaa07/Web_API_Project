1cea0855507511d89417b5e6b6539689b8d307c6
sushmitaa07 <sushmitapandey881@gmail.com>
2026-06-30 18:33:29 +0545

    endpoints

--- lib/api/endpoints.ts
+++ lib/api/endpoints.ts
@@ -6,5 +6,16 @@ export const API = {
         WHOAMI: "/api/v1/auth/whoami",
         UPDATE: "/api/v1/auth/update",
         UPDATE_PASSWORD: "/api/v1/auth/update-password",
-    }
+    },
+    ADMIN: {
+        USERS: "/api/v1/admin/users",
+        USER_BY_ID: (id: string) => `/api/v1/admin/users/${id}`,
+    },
+    ADMIN_BLOGS: {
+        LIST: "/api/v1/admin/blogs",
+        DETAIL: (id: string) => `/api/v1/admin/blogs/${id}`,
+        CREATE: "/api/v1/admin/blogs",
+        UPDATE: (id: string) => `/api/v1/admin/blogs/${id}`,
+        DELETE: (id: string) => `/api/v1/admin/blogs/${id}`,
+    },
 }
